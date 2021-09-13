ddfe81cab37f50de4456cbe6d9b69e34bc5efddb
ihollander <ianholla@gmail.com>
2021-09-13 10:41:14 -0400

    Fixed namespacing

--- lib/github_to_canvas_quiz/markdown_converter.rb
+++ lib/github_to_canvas_quiz/markdown_converter.rb
@@ -1,25 +1,27 @@
 # frozen_string_literal: true
 
-class CustomRender < Redcarpet::Render::HTML
-  include Rouge::Plugins::Redcarpet
-end
-
-class MarkdownConverter
-  attr_reader :markdown, :options
-
-  def initialize(markdown)
-    @markdown = markdown
-    @options = {
-      tables: true,
-      autolink: true,
-      fenced_code_blocks: true,
-      disable_indented_code_blocks: true,
-      no_intra_emphasis: true
-    }
+module GithubToCanvasQuiz
+  class CustomRender < Redcarpet::Render::HTML
+    include Rouge::Plugins::Redcarpet
   end
+  
+  class MarkdownConverter
+    attr_reader :markdown, :options
 
-  def to_html
-    renderer = CustomRender.new(escape_html: true)
-    Redcarpet::Markdown.new(renderer, options).render(markdown)
+    def initialize(markdown)
+      @markdown = markdown
+      @options = {
+        tables: true,
+        autolink: true,
+        fenced_code_blocks: true,
+        disable_indented_code_blocks: true,
+        no_intra_emphasis: true
+      }
+    end
+
+    def to_html
+      renderer = CustomRender.new(escape_html: true)
+      Redcarpet::Markdown.new(renderer, options).render(markdown)
+    end
   end
 end
