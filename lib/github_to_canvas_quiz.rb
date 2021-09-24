6e6db811422088746d6efb5169a92114cfeff78b
ihollander <ianholla@gmail.com>
2021-09-24 17:25:05 -0400

    Organize requires

--- lib/github_to_canvas_quiz.rb
+++ lib/github_to_canvas_quiz.rb
@@ -1,58 +1,51 @@
 # frozen_string_literal: true
 
-# API
-require 'rest-client'
+require 'front_matter_parser'
 require 'json'
-require_relative 'github_to_canvas_quiz/canvas_api/endpoints'
-require_relative 'github_to_canvas_quiz/canvas_api/client'
-
-# CLI
-require 'thor'
-require_relative 'github_to_canvas_quiz/cli'
-
-# Renderer Helpers
-require 'yaml'
-require 'reverse_markdown'
+require 'nokogiri'
 require 'redcarpet'
+require 'rest-client'
+require 'reverse_markdown'
 require 'rouge'
 require 'rouge/plugins/redcarpet'
-require_relative 'github_to_canvas_quiz/reverse_markdown/converters/pre'
-require_relative 'github_to_canvas_quiz/markdown_builder'
-require_relative 'github_to_canvas_quiz/markdown_converter'
+require 'thor'
+require 'yaml'
 
-# Parsers/Canvas
-require_relative 'github_to_canvas_quiz/parser/canvas/answer'
-require_relative 'github_to_canvas_quiz/parser/canvas/base'
-require_relative 'github_to_canvas_quiz/parser/canvas/question'
-require_relative 'github_to_canvas_quiz/parser/canvas/quiz'
+require 'github_to_canvas_quiz/builder/quiz'
 
-# Parsers/Markdown
-require 'front_matter_parser'
-require 'nokogiri'
-require_relative 'github_to_canvas_quiz/parser/markdown/helpers/node_parser'
-require_relative 'github_to_canvas_quiz/parser/markdown/helpers/node_scanner'
-require_relative 'github_to_canvas_quiz/parser/markdown/answer'
-require_relative 'github_to_canvas_quiz/parser/markdown/base'
-require_relative 'github_to_canvas_quiz/parser/markdown/question'
-require_relative 'github_to_canvas_quiz/parser/markdown/quiz'
+require 'github_to_canvas_quiz/canvas_api/endpoints/quizzes'
+require 'github_to_canvas_quiz/canvas_api/endpoints/quiz_questions'
+require 'github_to_canvas_quiz/canvas_api/endpoints'
+require 'github_to_canvas_quiz/canvas_api/client'
 
-# Models
-require_relative 'github_to_canvas_quiz/model/answer/fill_in_multiple_blanks'
-require_relative 'github_to_canvas_quiz/model/answer/matching'
-require_relative 'github_to_canvas_quiz/model/answer/multiple_answers'
-require_relative 'github_to_canvas_quiz/model/answer/multiple_choice'
-require_relative 'github_to_canvas_quiz/model/answer/short_answer'
-require_relative 'github_to_canvas_quiz/model/answer/true_false'
-require_relative 'github_to_canvas_quiz/model/quiz'
-require_relative 'github_to_canvas_quiz/model/question'
+require 'github_to_canvas_quiz/model/answer/fill_in_multiple_blanks'
+require 'github_to_canvas_quiz/model/answer/matching'
+require 'github_to_canvas_quiz/model/answer/multiple_answers'
+require 'github_to_canvas_quiz/model/answer/multiple_choice'
+require 'github_to_canvas_quiz/model/answer/short_answer'
+require 'github_to_canvas_quiz/model/answer/true_false'
+require 'github_to_canvas_quiz/model/quiz'
+require 'github_to_canvas_quiz/model/question'
 
-# Synchronizer
-require_relative 'github_to_canvas_quiz/synchronizer/quiz'
+require 'github_to_canvas_quiz/parser/canvas/answer'
+require 'github_to_canvas_quiz/parser/canvas/base'
+require 'github_to_canvas_quiz/parser/canvas/question'
+require 'github_to_canvas_quiz/parser/canvas/quiz'
+require 'github_to_canvas_quiz/parser/markdown/helpers/node_parser'
+require 'github_to_canvas_quiz/parser/markdown/helpers/node_scanner'
+require 'github_to_canvas_quiz/parser/markdown/answer'
+require 'github_to_canvas_quiz/parser/markdown/base'
+require 'github_to_canvas_quiz/parser/markdown/question'
+require 'github_to_canvas_quiz/parser/markdown/quiz'
 
-# Builder
-require_relative 'github_to_canvas_quiz/builder/quiz'
+require 'github_to_canvas_quiz/reverse_markdown/converters/pre'
 
-require_relative 'github_to_canvas_quiz/version'
+require 'github_to_canvas_quiz/synchronizer/quiz'
+
+require 'github_to_canvas_quiz/cli'
+require 'github_to_canvas_quiz/markdown_builder'
+require 'github_to_canvas_quiz/markdown_converter'
+require 'github_to_canvas_quiz/version'
 
 module GithubToCanvasQuiz
   class UnknownQuestionType < StandardError; end
--- lib/github_to_canvas_quiz/canvas_api/endpoints.rb
+++ lib/github_to_canvas_quiz/canvas_api/endpoints.rb
@@ -1,8 +1,5 @@
 # frozen_string_literal: true
 
-require_relative 'endpoints/quizzes'
-require_relative 'endpoints/quiz_questions'
-
 module GithubToCanvasQuiz
   module CanvasAPI
     module Endpoints
